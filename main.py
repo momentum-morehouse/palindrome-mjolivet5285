bc0edace0475e2bff26708d40369bb717dcb0d74
Jolivet <mjolivet@parkrowmedia.com>
2020-06-24 15:07:04 +0000

    added re.sub

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import re
+
 string = input('enter your string:')
-if(string == string[::-1]):
+house = re.sub(r'[^A-Za-z]','', string.lower())
+if house == house[::-1]:
   print('String is Palindome')
 else:
   print('String is not Palindome')
